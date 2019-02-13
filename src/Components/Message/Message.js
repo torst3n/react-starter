3427acc14f4a171488e313d01e68f7d5ece15c69
Thomas Liebetraut <thomas@tommie-lie.de>
2019-02-13 16:16:54 +0100

    Re-add styleguide command & config

--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -2,15 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Message.css";
 
+/**
+ * General component description in JSDoc format. Markdown is *supported*.
+ */
 const Message = (props) => {
   return (
-    <div className="message">
-      <p> {props.message} </p>
+    <div>
+      <p> {props.message || "Message"} </p>
     </div>
   );
 };
 
 Message.propTypes = {
+  /** A message to show */
   message: PropTypes.string
 };
+Message.defaultProps = {
+  message: "Some Message"
+}
+
 export default Message;
